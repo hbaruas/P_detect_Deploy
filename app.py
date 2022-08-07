1b47ce9f50ef041e738f840242e5825053ab6e64
saurabh <saurabhbaliyan000@gmail.com>
2022-08-07 18:09:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask import Flask, render_template,request
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.applications.vgg16 import decode_predictions
 from keras.models import load_model
 from keras_preprocessing import image
 import numpy as np
